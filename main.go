457c857965cd35746fc8cb25fe3a85f42c923d79
ricl <riclavaz@gmail.com>
2021-05-31 11:19:29 +0800

    cleanup

--- main.go
+++ main.go
@@ -20,34 +20,67 @@ import (
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
+// http
+const (
+	CONTENT_TYPE        = "Content-Type"
+	CONTENT_ENCODING    = "Content-Encoding"
+	CONTENT_DISPOSITION = "Content-Disposition"
+	GZIP                = "gzip"
+	MULTIPART_PREFIX    = "multipart/"
+	MULTIPART_BOUNDARY  = "boundary"
+)
+
+// env name
+const (
+	LISTEN_ADDR   = "LISTEN_ADDR"
+	S3_ENDPOINT   = "S3_ENDPOINT"
+	S3_ACCESSKEY  = "S3_ACCESSKEY"
+	S3_SECRETKEY  = "S3_SECRETKEY"
+	S3_BUCKETNAME = "S3_BUCKETNAME"
+	S3_IS_SSL     = "S3_IS_SSL"
+)
+
+// biz
+const (
+	DMP_FILE_KEY = "upload_file_minidump"
+	DMP          = ".dmp"
+	JSON         = ".json"
+)
+
+func getObjectNamePrefix(guid string) string {
+	// date - guid - uuid
+	current_time := time.Now()
+	time_str := fmt.Sprintf("%d-%d-%d-%d:%d:%d",
+		current_time.Year(), current_time.Month(), current_time.Day(),
+		current_time.Hour(), current_time.Minute(), current_time.Second())
+	id := uuid.New()
+	return fmt.Sprintf("%s-%s-%s", time_str, guid, id.String())
+}
+
+func werror(resp http.ResponseWriter, code int, err string) {
+	resp.WriteHeader(code)
+	resp.Write([]byte(err))
+	log.Println(err)
+}
+
 func handler(resp http.ResponseWriter, req *http.Request) {
 
-	compressed := strings.ToLower(req.Header.Get("Content-Encoding")) == "gzip"
+	compressed := strings.ToLower(req.Header.Get(CONTENT_ENCODING)) == GZIP
 
 	if compressed {
 		body, err := gzip.NewReader(req.Body)
 		if err != nil {
-			resp.WriteHeader(400)
-			resp.Write([]byte(err.Error()))
-			log.Println(err)
+			werror(resp, http.StatusBadRequest, err.Error())
 			return
 		}
 		defer body.Close()
 
-		mediaType, params, err := mime.ParseMediaType(req.Header.Get("Content-Type"))
+		mediaType, params, err := mime.ParseMediaType(req.Header.Get(CONTENT_TYPE))
 		if err != nil {
-			resp.WriteHeader(400)
-			resp.Write([]byte(err.Error()))
-			log.Println(err)
+			werror(resp, http.StatusBadRequest, err.Error())
 			return
 		}
 
-		// date - guid - uuid
-		current_time := time.Now()
-		time_str := fmt.Sprintf("%d-%d-%d-%d:%d:%d",
-			current_time.Year(), current_time.Month(), current_time.Day(),
-			current_time.Hour(), current_time.Minute(), current_time.Second())
-		id := uuid.New()
 		guid := ""
 		if val, ok := req.URL.Query()["guid"]; ok {
 			if len(val) > 0 {
@@ -55,74 +88,59 @@ func handler(resp http.ResponseWriter, req *http.Request) {
 			}
 		}
 		if guid == "" {
-			msg := "guid not present"
-			resp.WriteHeader(400)
-			resp.Write([]byte(msg))
-			log.Println(msg)
+			werror(resp, http.StatusBadRequest, "guid not present")
 			return
 		}
-		prefix := time_str + "-" + guid + "-" + id.String()
+		prefix := getObjectNamePrefix(guid)
 
 		metadata := make(map[string]string)
-		if strings.HasPrefix(mediaType, "multipart/") {
-			mr := multipart.NewReader(body, params["boundary"])
+		if strings.HasPrefix(mediaType, MULTIPART_PREFIX) {
+			mr := multipart.NewReader(body, params[MULTIPART_BOUNDARY])
 			for {
 				p, err := mr.NextPart()
 				if err == io.EOF {
 					break
 				}
 				if err != nil {
-					resp.WriteHeader(400)
-					resp.Write([]byte(err.Error()))
-					log.Println(err)
+					werror(resp, http.StatusInternalServerError, err.Error())
 					return
 				}
 				slurp, err := io.ReadAll(p)
 				if err != nil {
-					resp.WriteHeader(400)
-					resp.Write([]byte(err.Error()))
-					log.Println(err)
+					werror(resp, http.StatusInternalServerError, err.Error())
 					return
 				}
 
-				_, params, err := mime.ParseMediaType(p.Header.Get("Content-Disposition"))
+				_, params, err := mime.ParseMediaType(p.Header.Get(CONTENT_DISPOSITION))
 				if err != nil {
-					resp.WriteHeader(400)
-					resp.Write([]byte(err.Error()))
-					log.Println(err)
+					werror(resp, http.StatusBadRequest, "Could not parse meta from content disposition")
 					return
 				}
 
 				if val, ok := params["name"]; ok {
-					if val == "upload_file_minidump" {
+					if val == DMP_FILE_KEY {
 						// dmp
-						getClient().PutObject(context.Background(), getOrDefault("S3_BK", "crashpad"), prefix+".dmp", bytes.NewReader(slurp), -1, minio.PutObjectOptions{})
+						getClient().PutObject(context.Background(), getOrDefault(S3_BUCKETNAME, "crashpad"), prefix+DMP, bytes.NewReader(slurp), -1, minio.PutObjectOptions{})
 					} else {
 						// meta
 						metadata[val] = string(slurp)
 					}
 				} else {
-					msg := "name not present"
-					resp.WriteHeader(400)
-					resp.Write([]byte(msg))
-					log.Println(err)
+					werror(resp, http.StatusBadRequest, "name not present")
 					return
 				}
 			}
 		}
 
 		metadata_json, _ := json.MarshalIndent(metadata, "", "\t")
-		getClient().PutObject(context.Background(), getOrDefault("S3_BK", "crashpad"), prefix+".json", bytes.NewReader(metadata_json), -1, minio.PutObjectOptions{})
+		getClient().PutObject(context.Background(), getOrDefault(S3_BUCKETNAME, "crashpad"), prefix+JSON, bytes.NewReader(metadata_json), -1, minio.PutObjectOptions{})
 
 		resp.Write([]byte(prefix))
 		log.Println(prefix)
 		return
 	}
 
-	msg := "Invalid content format"
-	resp.WriteHeader(400)
-	resp.Write([]byte(msg))
-	log.Println(msg)
+	werror(resp, http.StatusBadRequest, "Invalid content format")
 }
 
 func getOrDefault(key, def string) string {
@@ -134,10 +152,10 @@ func getOrDefault(key, def string) string {
 }
 
 func getClient() *minio.Client {
-	endpoint := getOrDefault("S3_ENDPOINT", "play.min.io")
-	accessKeyID := getOrDefault("S3_AK", "Q3AM3UQ867SPQQA43P2F")
-	secretAccessKey := getOrDefault("S3_SK", "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG")
-	useSSL := getOrDefault("S3_SSL", "true") == "true"
+	endpoint := getOrDefault(S3_ENDPOINT, "play.min.io")
+	accessKeyID := getOrDefault(S3_ACCESSKEY, "Q3AM3UQ867SPQQA43P2F")
+	secretAccessKey := getOrDefault(S3_SECRETKEY, "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG")
+	useSSL := getOrDefault(S3_IS_SSL, "true") == "true"
 
 	// Initialize minio client object.
 	minioClient, err := minio.New(endpoint, &minio.Options{
@@ -152,6 +170,7 @@ func getClient() *minio.Client {
 
 func main() {
 	http.HandleFunc("/", handler)
-	fmt.Println("Server started at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	addr := getOrDefault(LISTEN_ADDR, ":8080")
+	log.Printf("Server started at port %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
